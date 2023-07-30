f978cc01a75b166b73b6caa3accbf97ccc7c0a4a
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2023-07-30 14:05:35 +1000

    License update

--- pkg/text.py
+++ pkg/text.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2023 Sai Kiran Gummaraj <saikiran.gummaraj@gmail.com>
+# Copyright 2023 Sai Kiran Gummaraj <opcoder0@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
